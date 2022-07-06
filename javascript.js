a04b01028b37bbcdf143f1b05b8f6411d7d13e93
Khwa Oo <khwalabear@gmail.com>
2022-07-06 14:36:22 -0400

    Added food/food location for snake

--- javascript.js
+++ javascript.js
@@ -1,12 +1,13 @@
 const gameArena = document.querySelector('section')
-const food = document.querySelector('.food')
 const newSnake = []
 const snake = []
 const arrow = 'Arrow'
-const foodEaten = false
 const gridSize = 784
 const centerSnake = gridSize / 2 + 14
+let deleteSnake = 0
+let foodEaten = false
 let addedSnake = centerSnake
+let rando = false
 
 for (let i = 0; i < gridSize; i++) {
   const createDiv = document.createElement('div')
@@ -20,12 +21,28 @@ const snakeSize = () => {
     snake.push(centerSnake)
   }
   for (let i = 0; i < snake.length; i++) {
-    boxId = snake
-    const snakeSquare = document.querySelector(`#box${snake[i]}`)
-    snakeSquare.style.backgroundColor = '#fff'
+    const locationSquare = document.querySelector(`#box${snake[i]}`)
+    locationSquare.style.backgroundColor = '#fff'
+    locationSquare.style.borderRadius = ''
   }
 }
-
+const foodLocation = () => {
+  let randNum = Math.floor(Math.random() * gridSize)
+  while (rando === false) {
+    snake.forEach((n) => {
+      rando = false
+      if (randNum === n) {
+        rando = false
+      } else {
+        rando = true
+      }
+    })
+  }
+  food = randNum
+  locationSquare = document.querySelector(`#box${randNum}`)
+  locationSquare.style.backgroundColor = '#4f7d5b'
+  locationSquare.style.borderRadius = '50%'
+}
 const upArrowPressed = () => {
   addedSnake -= 28
   growOrMove(addedSnake)
@@ -46,14 +63,20 @@ const rightArrowPressed = () => {
   growOrMove(addedSnake)
 }
 const growOrMove = (addedSnake) => {
-  if (foodEaten) {
-    ;('')
+  if (snake[snake.length - 1] === food) {
+    foodEaten = true
+    foodLocation()
   } else {
-    for (let i = 0; i < snake.length; i++) {
-      snakeSquare = document.querySelector(`#box${snake[i]}`)
-      snakeSquare.style.backgroundColor = '#f19c4d'
-      snake[i] = addedSnake
-    }
+    foodEaten = false
+  }
+  if (foodEaten) {
+    foodEaten = false
+    snake.push(addedSnake)
+  } else {
+    snake.push(addedSnake)
+    locationSquare = document.querySelector(`#box${snake.shift()}`)
+    locationSquare.style.backgroundColor = '#f19c4d'
+    locationSquare.style.borderRadius = ''
   }
   snakeSize()
 }
@@ -78,4 +101,5 @@ const arrowKeyPressed = (e) => {
   }
 }
 snakeSize()
+foodLocation()
 window.addEventListener('keydown', arrowKeyPressed)
